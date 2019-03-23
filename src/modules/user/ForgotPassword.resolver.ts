8b17f93f8c3f9dae6c08c3da7055d1d663140e44
Ivan <ivanakazver3d@gmail.com>
2019-03-23 14:57:07 +0700

    Change change-password link, remove ability to use forgot password while email not confirmed

--- src/modules/user/ForgotPassword.resolver.ts
+++ src/modules/user/ForgotPassword.resolver.ts
@@ -1,23 +1,22 @@
-import { UserInputError, ApolloError } from 'apollo-server-core';
-import { Resolver, Mutation, Arg, Ctx } from 'type-graphql';
+import { ApolloError, UserInputError } from 'apollo-server-core';
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
 import { v4 as uuid } from 'uuid';
-
+import { FORGOT_PASSWORD } from '../../constants/redisPrefixes';
 import { User } from '../../entity/User';
 import { IContext } from '../../types/IContext';
 import { sendForgotPasswordEmail } from '../../utils/sendForgotPasswordEmail';
-import { FORGOT_PASSWORD } from '../../constants/redisPrefixes';
 
 @Resolver()
 export class ForgotPasswordResolver {
   @Mutation(returns => Boolean)
   async forgotPassword(@Arg('email') email: string, @Ctx() { redis }: IContext): Promise<boolean> {
-    const user = await User.findOne({ where: { email }, select: ['id'] });
+    const user = await User.findOne({ where: { email, confirmed: true }, select: ['id'] });
     if (!user) {
       throw new UserInputError('User with that email does not exist.');
     }
     const token = uuid();
-    await redis.set(`${FORGOT_PASSWORD}${token}`, user.id, 'ex', 60 * 20); // 20 minutes
-    const url = `http://localhost:3000/change-password/${token}`;
+    await redis.set(`${FORGOT_PASSWORD}${token}`, user.id, 'ex', 60 * 60); // 60 minutes
+    const url = `http://localhost:3000/user/change-password/${token}`;
     try {
       await sendForgotPasswordEmail(email, url);
     } catch {
--- src/modules/user/Me.resolver.ts
+++ src/modules/user/Me.resolver.ts
@@ -1,6 +1,5 @@
-import { Resolver, Ctx, Query } from 'type-graphql';
 import { AuthenticationError, UserInputError } from 'apollo-server-core';
-
+import { Ctx, Query, Resolver } from 'type-graphql';
 import { User } from '../../entity/User';
 import { IContext } from '../../types/IContext';
 
@@ -9,11 +8,11 @@ export class MeResolver {
   @Query(returns => User)
   async me(@Ctx() { req }: IContext): Promise<User> {
     if (!req.session || !req.session.userId) {
-      throw new AuthenticationError('You are not authenticated.');
+      throw new AuthenticationError('not authenticated');
     }
     const user = await User.findOne(req.session.userId);
     if (!user) {
-      throw new UserInputError('Invalid session');
+      throw new UserInputError('not authenticated');
     }
     return user;
   }
