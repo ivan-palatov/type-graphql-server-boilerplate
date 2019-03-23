5745416cca04ef822fd959c0c19f08c4ee9c9550
Ivan <ivanakazver3d@gmail.com>
2019-03-23 13:11:40 +0700

    Change confirm email link

--- src/utils/createConfirmationUrl.ts
+++ src/utils/createConfirmationUrl.ts
@@ -1,11 +1,10 @@
 import { Redis } from 'ioredis';
 import { v4 as uuid } from 'uuid';
-
 import { CONFIRM_EMAIL } from '../constants/redisPrefixes';
 
 export const createConfirmationUrl = async (userId: number, redis: Redis) => {
-  const id = uuid();
-  await redis.set(`${CONFIRM_EMAIL}${id}`, userId, 'ex', 60 * 60 * 24); //1 day
-  
-  return `http://localhost:3000/confirm/${id}`;
+  const token = uuid();
+  await redis.set(`${CONFIRM_EMAIL}${token}`, userId, 'ex', 60 * 60 * 24); //1 day
+
+  return `http://localhost:3000/user/confirm/${token}`;
 };
